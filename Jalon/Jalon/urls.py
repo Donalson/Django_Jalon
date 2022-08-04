7c58a1207b7856ebb01327a7d16408d56dbf7df9
Donalson <donalson1517@gmail.com>
2022-08-03 23:00:18 -0600

    Estructura base de index
    Se creo una estructura base del index.html, se realizaron unos ajustes de ruta y unas correciones de colores

--- Jalon/Jalon/urls.py
+++ Jalon/Jalon/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Jalon import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='Index'),
 
     path('', include('Login.urls'))
 ]
--- Jalon/Jalon/views.py
+++ Jalon/Jalon/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+
+def index(request):
+    usuario = {'username':'Donalson', 'category':'admin'}
+    return render(request, 'Jalon/index.html', {'usuario':usuario})
