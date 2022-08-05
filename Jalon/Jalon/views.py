4068eba56b6abec09b853af7e8f4ad4a5f1bcae0
Donalson <donalson1517@gmail.com>
2022-08-04 22:17:25 -0600

    Protecion de vistas
    Se utilizo el decorador para proteger las vistas

--- Jalon/Jalon/views.py
+++ Jalon/Jalon/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login/')
 def index(request):
     usuario = {'username':'Donalson', 'category':'admin'}
     return render(request, 'Jalon/index.html', {'usuario':usuario})
