a09ace9409c9ce4d18879b12a519c4c1171b2297
Donalson <donalson1517@gmail.com>
2022-08-05 00:16:37 -0600

    Prueba de manejo de rutas y configuracion de rutas
    Se añadio la configuracion  y manejo de urls correspondiente para mostrar archivos en el servidor de pruebas, ademas de rutas personalizadas para los archivos de usuario

--- Jalon/Usuarios/models.py
+++ Jalon/Usuarios/models.py
@@ -1,6 +1,37 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+def ruta_personalizada_foto(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Foto.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+
+def ruta_personalizada_Dpi(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Dpi.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+
+def ruta_personalizada_P1(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Pose1.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+
+def ruta_personalizada_P2(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Pose2.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+
+def ruta_personalizada_S1(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Selfie1.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+
+def ruta_personalizada_S2(instance, filename):
+    ext = filename.split('.')[-1]
+    Foto = 'Selfie2.{}'.format(ext)
+    return 'usuario_{0}/{1}'.format(instance.id, Foto)
 
 class Usuario(models.Model):
     Usuario = models.CharField(max_length=50)
@@ -8,14 +39,14 @@ class Usuario(models.Model):
     Categoria = models.IntegerField(blank=True, null=True)
     Usuario_FK = models.IntegerField(blank=True, null=True)
     Nombre_Completo = models.CharField(max_length=50)
-    Telefono = models.CharField(max_length=10, blank=True, null=True)
-    Foto = models.ImageField()
+    Telefono = models.CharField(max_length=10, blank=True, null=True,)
+    Foto = models.ImageField(upload_to=ruta_personalizada_foto, null=True, blank=True)
     Verificado = models.BooleanField(default=False)
-    Pose1 = models.ImageField()
-    Pose2 = models.ImageField()
-    Selfie1 = models.ImageField()
-    Selfie2 = models.ImageField()
-    Dpi = models.ImageField()
+    Pose1 = models.ImageField(upload_to=ruta_personalizada_P1, null=True, blank=True)
+    Pose2 = models.ImageField(upload_to=ruta_personalizada_P2, null=True, blank=True)
+    Selfie1 = models.ImageField(upload_to=models.ImageField(upload_to=ruta_personalizada_S1, null=True, blank=True), null=True, blank=True)
+    Selfie2 = models.ImageField(upload_to=ruta_personalizada_S2, null=True, blank=True)
+    Dpi = models.ImageField(upload_to=ruta_personalizada_Dpi, null=True, blank=True)
     Mensaje = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
