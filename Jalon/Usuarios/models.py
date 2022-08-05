691606f905249563003f58817fe1dc2bf64892bf
Donalson <donalson1517@gmail.com>
2022-08-05 01:13:48 -0600

    Solucion de errores
    Se solucion un error en la configuracion de ruta donde se borro una coma demas, se añadio la carpeta media a las excepciones de git y se cambio el nombre de las carpetas de usuarios de id por nombre de usuario

--- Jalon/Usuarios/models.py
+++ Jalon/Usuarios/models.py
@@ -6,35 +6,41 @@ from django.db import models
 def ruta_personalizada_foto(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Foto.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 def ruta_personalizada_Dpi(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Dpi.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 def ruta_personalizada_P1(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Pose1.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 def ruta_personalizada_P2(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Pose2.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 def ruta_personalizada_S1(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Selfie1.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 def ruta_personalizada_S2(instance, filename):
     ext = filename.split('.')[-1]
     Foto = 'Selfie2.{}'.format(ext)
-    return 'usuario_{0}/{1}'.format(instance.id, Foto)
+    ruta = 'usuario_%s/%s'%(instance.Usuario, Foto)
+    return ruta
 
 class Usuario(models.Model):
-    Usuario = models.CharField(max_length=50)
+    Usuario = models.CharField(max_length=50, unique=True)
     Contraseña = models.CharField(max_length=50)
     Categoria = models.IntegerField(blank=True, null=True)
     Usuario_FK = models.IntegerField(blank=True, null=True)
@@ -44,7 +50,7 @@ class Usuario(models.Model):
     Verificado = models.BooleanField(default=False)
     Pose1 = models.ImageField(upload_to=ruta_personalizada_P1, null=True, blank=True)
     Pose2 = models.ImageField(upload_to=ruta_personalizada_P2, null=True, blank=True)
-    Selfie1 = models.ImageField(upload_to=models.ImageField(upload_to=ruta_personalizada_S1, null=True, blank=True), null=True, blank=True)
+    Selfie1 = models.ImageField(upload_to=ruta_personalizada_S1, null=True, blank=True)
     Selfie2 = models.ImageField(upload_to=ruta_personalizada_S2, null=True, blank=True)
     Dpi = models.ImageField(upload_to=ruta_personalizada_Dpi, null=True, blank=True)
     Mensaje = models.TextField(blank=True, null=True)
