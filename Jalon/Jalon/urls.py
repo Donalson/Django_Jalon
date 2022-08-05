5bd7ddb19e442b4298b8364fba60e34cc315283c
Donalson <donalson1517@gmail.com>
2022-08-05 15:11:07 -0600

    Pequeños arreglos
    Se cambio la sintaxis de la importacion de vistas y se arreglo el indicar de los links de la barra de navegacion

--- Jalon/Jalon/urls.py
+++ Jalon/Jalon/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Jalon import views
+from . import views
 #Importaciones para mostrar imagenes en el servidor de pruebas
 from django.conf import settings
 from django.conf.urls.static import static
--- Jalon/Usuarios/urls.py
+++ Jalon/Usuarios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Usuarios import views
+from . import views
 
 urlpatterns = [
     path('usuarios/', views.usuarios, name='Usuarios'),
