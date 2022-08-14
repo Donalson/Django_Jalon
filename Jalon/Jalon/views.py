49eb2775c51484dd0062c25dec51b07466799493
Donalson <donalson1517@gmail.com>
2022-08-13 20:24:11 -0600

    Pequeños arreglos
    Se realizaron unos ajustes al detalle de usuario y se añadio una funcionalidad minima al index para no dejarlo vacio

--- Jalon/Jalon/views.py
+++ Jalon/Jalon/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Usuarios.models import Usuario
+from Depositos.models import Depositos
 
 @login_required(login_url='login/')
 def index(request):
-    usuario = {'username':'Donalson', 'category':'admin'}
-    return render(request, 'Jalon/index.html', {'usuario':usuario})
+    Usuarios_Pedientes = len(Usuario.objects.filter(Verificado=None))
+    Depositos_Pedientes = len(Depositos.objects.filter(Depositado=None))
+    return render(request, 'Jalon/index.html', {'Usuarios':Usuarios_Pedientes, 'Depositos':Depositos_Pedientes})
